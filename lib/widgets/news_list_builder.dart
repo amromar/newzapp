9cf340eeba6116379dbcaad5201c59dc30a7ec42
AMR.OMAR <amro.omar@proton.me>
2024-04-03 10:57:13 +0200

    FutureBuilder

--- lib/widgets/news_list_builder.dart
+++ lib/widgets/news_list_builder.dart
@@ -1,71 +1,62 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:newzapp/models/article_model.dart';
+import 'package:newzapp/services/news_service.dart';
 import 'package:newzapp/widgets/content_loader_widget.dart';
-import '../models/article_model.dart';
-import '../services/news_service.dart';
 import 'news_tile_widget.dart';
 
 class NewsListBuilder extends StatefulWidget {
+  final Key? key;
 
-   NewsListBuilder({super.key});
+  NewsListBuilder({this.key}) : super(key: key);
 
   @override
   State<NewsListBuilder> createState() => _NewsListBuilderState();
 }
 
 class _NewsListBuilderState extends State<NewsListBuilder> {
-
-  bool isLoading = true;
-  List<ArticleModel> articles = [];
-  @override
-  void initState()  {
-    super.initState();
-    initNews();
-
-  }
-  Future<void> initNews() async {
-    List<ArticleModel> articles= await NewsService(Dio()).getGeneralNews();
-    isLoading = false;
-    setState(() {
-      this.articles = articles;
-    });
-  }
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-    return isLoading
-        ? SliverFillRemaining(
-      child: ContentLoader(),
-    )
-        : articles.isEmpty
-        ? SliverToBoxAdapter(
-      child: Center(
-        child: Container(
-          alignment: Alignment.center,
-          height: height / 2,
-          child: const Text(
-            'No data found',
-            style: TextStyle(
-              color: Colors.orange,
-              fontSize: 20,
-            ),
-          ),
-        ),
-      ),
-    )
-        : SliverList(
-      delegate: SliverChildBuilderDelegate(
-            (context, index) {
-          return Padding(
-            padding: const EdgeInsets.only(top: 14),
-            child: NewsTileWidget(
-              articleModel: articles[index],
+    return FutureBuilder<List<ArticleModel>>(
+      future: NewsService(Dio()).getGeneralNews(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return SliverToBoxAdapter(
+            child: Center(
+              child: Container(
+                alignment: Alignment.center,
+                height: MediaQuery.of(context).size.height / 2,
+                child: CircularProgressIndicator(
+                  color: Colors.orange,
+                ),
+              ),
             ),
           );
-        },
-        childCount: articles.length,
-      ),
+        } else if (snapshot.hasError) {
+          return SliverToBoxAdapter(
+            child: Center(
+              child: Container(
+                alignment: Alignment.center,
+                height: MediaQuery.of(context).size.height / 2,
+                child: Text('Error Loading Data', style: TextStyle(color: Colors.orange) ,),
+              ),
+            ),
+          );
+        } else {
+          return SliverList(
+            delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(top: 14),
+                  child: NewsTileWidget(articleModel: snapshot.data![index]),
+                );
+              },
+              childCount: snapshot.data!.length,
+            ),
+          );
+        }
+      },
     );
   }
 }
--- lib/widgets/news_tile_widget.dart
+++ lib/widgets/news_tile_widget.dart
@@ -18,14 +18,14 @@ class NewsTileWidget extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           ClipRRect(
-            // roundedn image container
+            // rounded image container
               borderRadius: BorderRadius.circular(10),
               child: Image(image: NetworkImage (articleModel.imageUrl??'https://images.unsplash.com/photo-1504711434969-e33886168f5c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'), height: 200, width: 400, fit: BoxFit.cover)),
           Padding(
             padding: const EdgeInsets.all(10.0),
             child: Column(
 
-              children: [
+            children: [
                 Text(articleModel.title ?? '', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold , ),textAlign: TextAlign.left, maxLines: 1,overflow: TextOverflow.ellipsis,),
                 Text(articleModel.description??'No Data', style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal ,color: Colors.grey ),textAlign: TextAlign.left, maxLines: 2, overflow: TextOverflow.ellipsis),
               ],
